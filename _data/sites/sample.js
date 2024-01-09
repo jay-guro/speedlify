8da9ba63cecebae569eceb82a37d2c11a788e0f9
Jason Guro <hello@jasonguro.com>
2024-01-09 00:13:34 -0500

    1st commit

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,11 +1,28 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
+	name: "Jason's sites", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		runs: 1,
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://samuelrodriguezc.vercel.app",
+		"https://www.curatedtraveldestinations.com",
+		"https://www.28-de-julio.com",
+		"https://extended-fam.github.io",
+		"https://www.lajollacondominio.com",
+		"https://www.robsbroccoli.com",
+		"https://www.portahomes.co.nz",
+		"https://www.zabari.pe",
+		"https://www.mde.rentals",
+		"https://www.elparchecolombianfastfood.com",
+		"https://www.highroad207.com",
+		"https://www.turbocarguy.com",
+		"https://www.colvisahelp.com",
+		"https://sherpas.dev",
+		"https://www.floridaresidence.us",
+		"https://www.hellooki.com",
+		"https://www.campusdeiaguaviva.com",
+		"https://www.alexroman.boats",
 	]
 };
